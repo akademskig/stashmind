4d1891e388d0d2f073b7eaa48dbfbb931c77bfef
marta <m.susek@live.com>
2025-04-04 08:39:21 +0200

    dev: change import path shorthand

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,9 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Geist } from "next/font/google";
 
-import { api } from "@/utils/api";
+import { api } from "~/utils/api";
 
-import "@/styles/globals.css";
+import "~/styles/globals.css";
 
 const geist = Geist({
   subsets: ["latin"],
